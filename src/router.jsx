df5d6f35c61561d2ad3ca713324503ba341e4232
agybay-it <agybaymukhametzhanbusiness@gmail.com>
2026-05-10 22:20:26 +0500

    feat: update workflow

--- src/router.jsx
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter } from "react-router-dom";
-import { App } from "./pages/home/App.jsx";
-
-export const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <App />
-	}
-]);
